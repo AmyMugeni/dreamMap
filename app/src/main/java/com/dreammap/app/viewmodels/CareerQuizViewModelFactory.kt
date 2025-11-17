88a6412411c4c52e77d41a102bc2da5d2326fd80
Tedd Onyesero <teddonyesero7@gmail.com>
2025-11-17 03:26:15 +0300

    feat: Update CareerQuizViewModelFactory to include roadmapRepository
    - Modify CareerQuizViewModelFactory to accept roadmapRepository as a parameter
    - Update ViewModel creation to pass both userRepository and roadmapRepository

--- app/src/main/java/com/dreammap/app/viewmodels/CareerQuizViewModelFactory.kt
+++ app/src/main/java/com/dreammap/app/viewmodels/CareerQuizViewModelFactory.kt
@@ -2,15 +2,17 @@ package com.dreammap.app.viewmodels
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.dreammap.app.data.repositories.RoadmapRepository
 import com.dreammap.app.data.repositories.UserRepository
 
 class CareerQuizViewModelFactory(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val roadmapRepository: RoadmapRepository
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CareerQuizViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return CareerQuizViewModel(userRepository) as T
+            return CareerQuizViewModel(userRepository, roadmapRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
